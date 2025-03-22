cebdd0af1d4dbc5abeee03b42e6fb793c0baed97
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-03-22 16:45:49 -0400

    Stoping and wait for coroutines to finish

--- Assets/Scripts/CoroutineGrower.cs
+++ Assets/Scripts/CoroutineGrower.cs
@@ -10,30 +10,48 @@ public class CoroutineGrower : MonoBehaviour
     public float maxSize = 1;
     public float t;
     public Transform apple;
-    public void StartGrow()
-    {
+    public Transform cookie;
+    //public void StartGrow()
+    //{
 
-        StartCoroutine(Grow());
+    //    StartCoroutine(Grow());
 
-    }
+    //}
 
 
-    IEnumerator Grow()
+    public IEnumerator Grow()
     {
         apple.localScale = Vector3.zero;
+        cookie.localScale = Vector3.zero;
+        transform.localScale = Vector3.zero;
+        // grow the tree
         t = 0;
         //transform.localScale = Vector3.Lerp(Vector3.one * minSize, Vector3.one * maxSize, curve.Evaluate(t));
+        yield return new WaitForSeconds(Random.Range(.05f, .5f));
         while (t < 1)
         {
             t += Time.deltaTime;
             transform.localScale = Vector3.one * maxSize * curve.Evaluate(t);
             yield return null;
         }
+
+        //grow the apple
         t = 0;
         while (t < 1)
         {
             t += Time.deltaTime;
             apple.localScale = Vector3.one * maxSize * curve.Evaluate(t);
+            yield return null;
+        }
+        // 2 could also be replaced with a Random.Range(.05f, .5f) for random wait time
+        yield return new WaitForSeconds(Random.Range(.05f, .5f));
+
+        //grow the cookie
+        t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            cookie.localScale = Vector3.one * maxSize * curve.Evaluate(t);
             yield return null;
         }
 
