11ae6e985db7429fa6a7d71626226989d06489e5
Anthony Pattison <pattisoa@shernet.sheridancollege.ca>
2025-03-26 13:18:57 -0400

    more coding gym, the heart rate one completed

--- Assets/Scripts/Batmovement.cs
+++ Assets/Scripts/Batmovement.cs
@@ -6,6 +6,15 @@ public class Batmovement : MonoBehaviour
 {
     public GameObject Sword;
     public float speed;
+    public float t = 1;
+
+    public AnimationCurve curve;
+
+    Vector2 Beginpos;
+    Vector2 Endpos;
+
+    public Vector2 pos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +25,35 @@ public class Batmovement : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
+        Vector2 temppos = new Vector2(0,0);
         Vector2 screenpos = Camera.main.WorldToScreenPoint(pos);
 
-        Debug.Log(speed);
-;        if (pos.x <= 0 || pos.x >= Screen.width)
+        if (t>=1)
         {
-           
-            speed = speed * -1;
+            Beginpos = new Vector2(pos.x, pos.y);
+            Endpos = new Vector2(pos.x, pos.y + 5);
+            t = 0;
+            Debug.Log("working");
         }
-        pos.x += speed;
+        if (t < 1)
+        {
+            t += 0.8f * Time.deltaTime;
+            temppos = Vector2.Lerp(Beginpos, Endpos, curve.Evaluate(t));
+            pos.y = temppos.y;
+        }
+;        if (screenpos.x >= Screen.width)
+        {
+            
+            screenpos.x = 0;
+            pos.x -= (pos.x * 2);
+            transform.position = pos;
+            GetComponent<TrailRenderer>().Clear();
+        }
+        pos.x += speed * Time.deltaTime;
         transform.position = pos;
+       
     }
+
+    
+    
 }
